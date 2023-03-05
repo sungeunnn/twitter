21ef9ecad8607f9fd1509d374778ee0173211623
sungeunnn <fronteun@naver.com>
2023-03-05 21:53:58 +0900

    [feat] tweets 읽어오기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import firebase from "firebase/compat/app";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+// import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,40 @@
 import { dbService } from "fbase";
-import {addDoc, collection} from "firebase/firestore"
-import React, { useState } from "react";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
-  const onSubmit = async(e) => {
+  const [tweets, setTweets] = useState([]);
+
+  const getTweets = async () => {
+    const q = query(collection(dbService, "tweets"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      const tweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setTweets((prev) => [tweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
+
+  const onSubmit = async (e) => {
     e.preventDefault();
     try {
       const docRef = await addDoc(collection(dbService, "tweets"), {
-      tweet,
-      createdAt: Date.now(),
+        tweet,
+        createdAt: Date.now(),
       });
       console.log("Document written with ID: ", docRef.id);
-      } catch (error) {
+    } catch (error) {
       console.error("Error adding document: ", error);
-      }
+    }
     setTweet("");
   };
+
   const onChange = (e) => {
     const {
       target: { value },
@@ -25,10 +43,17 @@ const Home = () => {
   };
   return (
     <div>
-      <form onClick={onSubmit}>
+      <form onSubmit={onSubmit}>
         <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Tweet" />
       </form>
+      <div>
+        {tweets.map((tw) => (
+          <div key={tw.id}>
+            <h4>{tw.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
