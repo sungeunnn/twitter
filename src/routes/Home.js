52a730d5bf9cf8a8264bb1e922ccfe6cbc589204
sungeunnn <fronteun@naver.com>
2023-03-05 21:19:07 +0900

    [feat] Cloud Firestore에 tweets 추가하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,35 @@
-import React from 'react';
+import { dbService } from "fbase";
+import {addDoc, collection} from "firebase/firestore"
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+      tweet,
+      createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+      } catch (error) {
+      console.error("Error adding document: ", error);
+      }
+    setTweet("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onClick={onSubmit}>
+        <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
