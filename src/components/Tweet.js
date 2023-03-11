be07bd7d335bd82f840f73c36f740e8dbe0ce21a
sungeunnn <fronteun@naver.com>
2023-03-11 23:56:26 +0900

    [feat] tweet 삭제, 수정 기능 구현

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,14 +1,58 @@
-import React from "react";
-const Tweet = ({ tweetObj, isOwner }) => (
-  <div>
-    <h4>{tweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete</button>
-        <button>Edit</button>
-      </>
-    )}
-  </div>
-);
+import React, { useState } from "react";
+import { dbService } from "fbase";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+
+const Tweet = ({ tweetObj, isOwner }) => {
+  const TweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
+
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("트윗을 지우겠습니까?");
+    if (ok) {
+      await deleteDoc(TweetTextRef);
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    console.log(tweetObj, newTweet);
+    await updateDoc(TweetTextRef, {
+      text: newTweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewTweet(value);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="text" placeholder="Edit your tweet" value={newTweet} required />
+            <button onClick={toggleEditing}>Cancel</button>
+            <input type="submit" value="Update Tweet" />
+          </form>
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete</button>
+              <button onClick={toggleEditing}>Edit</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Tweet;
